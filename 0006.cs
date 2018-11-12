1bfe7afdb089b5216b05f2f25ac39b2e086230cb
Andrew Stephenson <andrewjohnroy@gmail.com>
2018-11-12 11:58:31 +1100

    add 0006

---
+++ 0006.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Project_Euler_Solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumofsquares = 0;
+			int squareofsum = 0;
+			
+			for (int i = 1; i <= 100; i++){
+				sumofsquares += i * i;
+				squareofsum += i;
+			}
+
+			squareofsum *= squareofsum;
+			Console.WriteLine(squareofsum - sumofsquares);
+        }
+    }
+}
